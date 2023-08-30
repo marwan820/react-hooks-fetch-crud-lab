a3f3e08a753da58d9bfcb506686a0eeee6436cf2
Marwan Rahim <marwan820@Marwan>
2023-08-29 19:35:49 -0500

    fetched data with useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [page, setPage] = useState("List")
+
+
+const getQuestions = (questionsData) => {
+
+  questionsData.map((questionElement) => <li key={questionElement.id}>{questionElement.prompt}</li>)
+
+
+
+}
+
+
+
+
+
+  
+  
+  useEffect(() => {
+fetch(`http://localhost:4000/questions`)
+.then(response => response.json())
+.then(questionsData => getQuestions(questionsData))
+})
+
 
   return (
     <main>
@@ -13,5 +35,6 @@ function App() {
     </main>
   );
 }
+
 
 export default App;
